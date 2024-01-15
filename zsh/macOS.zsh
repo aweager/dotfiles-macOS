15dd492cd13c12b9078f2eee0854ddeb6af04862
Alexander Eager <alexandereager@Alexanders-MacBook-Air.local>
2024-01-15 00:30:18 -0800

    Initial macOS dotfiles commit

---
+++ zsh/macOS.zsh
@@ -0,0 +1,9 @@
+####### macOS zsh config
+
+# zplug
+export ZPLUG_HOME=/opt/homebrew/opt/zplug
+source "$ZPLUG_HOME/init.zsh"
+
+# aliases
+alias chrome="/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
+alias ssha="ssh-add --apple-load-keychain"
