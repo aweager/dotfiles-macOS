2db24448f5630c7d3da6693d2e6bbe5edb8590df
Alex Eager <156651956+aweager@users.noreply.github.com>
2024-06-15 21:27:37 -0700

    no more zplug

--- zsh/macOS.zsh
+++ zsh/macOS.zsh
@@ -1,9 +1,5 @@
 ####### macOS zsh config
 
-# zplug
-export ZPLUG_HOME=/opt/homebrew/opt/zplug
-source "$ZPLUG_HOME/init.zsh"
-
 # aliases
 alias chrome="/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome"
 alias ssha="ssh-add --apple-load-keychain"
